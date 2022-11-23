ab366a8bef66a3b56cbfb94fa22a4814be9efbc2
WesleyMatthee <wrmatthee@gmail.com>
2022-11-23 20:39:51 +0000

    Added name to client

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 // establishes a connection with the game server
 const net = require("net");
 
+
 const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
@@ -9,9 +10,14 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  //This connect event logs the data sent from the server.
   conn.on("data", (data) => {
     console.log(data);
+  });
+  //
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: WRM");
   });
 
   return conn;
