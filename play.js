ba072fe8928efda8e4a8ff7506a87de5a8a670a7
WesleyMatthee <wrmatthee@gmail.com>
2022-11-23 21:29:51 +0000

    Added a key command to exit the game

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  
+  
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  //'\u0003' is cntl+c which exits the server
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
 
 console.log('Connecting ...');
 connect();
